16dc0d555757260b01d1d25ca52e5c09145f307d
Lewis <lmann.8602@gmail.com>
2024-11-08 11:56:48 +0000

    components 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hw).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, hw)
+        .run();
 }
 
 fn hw() {
     println!("ey up");
 }
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Lewis".to_string())));
+    commands.spawn((Person, Name("Zumbu".to_string())));
+    commands.spawn((Person, Name("Hume".to_string())));
+} 
