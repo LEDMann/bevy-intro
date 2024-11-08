ca56d5a023454c0cae606653517b211637177777
Lewis <lmann.8602@gmail.com>
2024-11-08 12:25:51 +0000

    resources 1

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
-        app.add_systems(Update, (hw, (update_people, greet_people).chain()));
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
-fn hw() {
-    println!("ey up");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 #[derive(Component)]
 struct Person;
@@ -32,9 +32,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Hume".to_string())));
 } 
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
